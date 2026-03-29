7863bb9d038afc7229239b0a4508e0d115ab6a1b
o.barylo <oleksiib@backbase.com>
2026-03-30 02:19:46 +0300

    feat: Move Sulfuras-related unit tests from GildedRoseTest to SulfurasUpdateStrategyTest.

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -10,43 +10,6 @@ import org.junit.jupiter.api.Test;
 class GildedRoseTest {
 
     @Nested
-    @DisplayName("Sulfuras suite")
-    class Sulfuras {
-
-        private static final String SULFURAS_NAME = "Sulfuras, Hand of Ragnaros";
-
-        @Test
-        void sellInNeverDecreases() {
-            Item[] items = new Item[] { new Item(SULFURAS_NAME, 15, 80) };
-
-            new GildedRose(items).updateQuality();
-
-            assertEquals(15, items[0].sellIn);
-            assertEquals(80, items[0].quality);
-        }
-
-        @Test
-        void qualityNeverDecreases() {
-            Item[] items = new Item[] { new Item(SULFURAS_NAME, -1, 80) };
-
-            new GildedRose(items).updateQuality();
-
-            assertEquals(-1, items[0].sellIn);
-            assertEquals(80, items[0].quality);
-        }
-
-        @Test
-        void qualityConstantValueIsEighty() {
-            Item[] items = new Item[] { new Item(SULFURAS_NAME, 0, 80) };
-
-            new GildedRose(items).updateQuality();
-
-            assertEquals(0, items[0].sellIn);
-            assertEquals(80, items[0].quality);
-        }
-    }
-
-    @Nested
     @DisplayName("Conjured item suite")
     class ConjuredItem {
 
