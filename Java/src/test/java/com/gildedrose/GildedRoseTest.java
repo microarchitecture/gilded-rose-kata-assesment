97c6ad87470ad899eba0d22d00c53b1bf2726924
o.barylo <oleksiib@backbase.com>
2026-03-29 16:15:58 +0300

    feat: add Normal Item unit tests implementation.

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -10,20 +12,45 @@ class GildedRoseTest {
     @DisplayName("Normal Item suite")
     class NormalItem {
 
+        private static final String NORMAL_ITEM_NAME = "Normal Item";
+
         @Test
         void qualityDegradesByOneBeforeSellByDate() {
+            Item[] items = new Item[] { new Item(NORMAL_ITEM_NAME, 5, 10) };
+
+            new GildedRose(items).updateQuality();
+
+            assertEquals(4, items[0].sellIn);
+            assertEquals(9, items[0].quality);
         }
 
         @Test
         void qualityDegradesTwoTimesFasterAfterSellByDate() {
+            Item[] items = new Item[] { new Item(NORMAL_ITEM_NAME, 0, 10) };
+
+            new GildedRose(items).updateQuality();
+
+            assertEquals(-1, items[0].sellIn);
+            assertEquals(8, items[0].quality);
         }
 
         @Test
         void qualityMinimumValueIsZero() {
+            Item[] items = new Item[] { new Item(NORMAL_ITEM_NAME, 5, 0) };
+
+            new GildedRose(items).updateQuality();
+
+            assertEquals(4, items[0].sellIn);
+            assertEquals(0, items[0].quality);
         }
 
         @Test
         void qualityMaximumValueIsFifty() {
+            Item[] items = new Item[] { new Item(NORMAL_ITEM_NAME, 5, 50) };
+
+            new GildedRose(items).updateQuality();
+
+            assertEquals(49, items[0].quality);
         }
     }
 
