fe4acc3d14ee39371e54792fedaa64ac80762351
o.barylo <oleksiib@backbase.com>
2026-03-30 01:05:26 +0300

    feat: Add NormalUpdateStrategy implementation for normal items.
    Add ItemRuleConstants.

---
+++ Java/src/main/java/com/gildedrose/ItemRuleConstants.java
@@ -0,0 +1,14 @@
+package com.gildedrose;
+
+/**
+ * Constants that are applicable for {@link Item} rules.
+ */
+public final class ItemRuleConstants {
+
+    public static final int QUALITY_MIN_VALUE = 0;
+    public static final int QUALITY_MAX_VALUE = 50;
+    public static final int QUALITY_DEFAULT_CHANGE = 1;
+
+    private ItemRuleConstants() {
+    }
+}
