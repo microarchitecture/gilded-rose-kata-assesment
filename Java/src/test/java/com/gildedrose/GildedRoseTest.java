87ade25be6161ad8f6d43190eff7bcb62d8e8f3a
o.barylo <oleksiib@backbase.com>
2026-03-29 17:36:13 +0300

    feat: add Sulfuras item unit tests implementation.

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -162,16 +162,36 @@ class GildedRoseTest {
     @DisplayName("Sulfuras suite")
     class Sulfuras {
 
+        private static final String SULFURAS_NAME = "Sulfuras, Hand of Ragnaros";
+
         @Test
         void sellInNeverDecreases() {
+            Item[] items = new Item[] { new Item(SULFURAS_NAME, 15, 80) };
+
+            new GildedRose(items).updateQuality();
+
+            assertEquals(15, items[0].sellIn);
+            assertEquals(80, items[0].quality);
         }
 
         @Test
         void qualityNeverDecreases() {
+            Item[] items = new Item[] { new Item(SULFURAS_NAME, -1, 80) };
+
+            new GildedRose(items).updateQuality();
+
+            assertEquals(-1, items[0].sellIn);
+            assertEquals(80, items[0].quality);
         }
 
         @Test
-        void qualityMaximumValueIsEighty() {
+        void qualityConstantValueIsEighty() {
+            Item[] items = new Item[] { new Item(SULFURAS_NAME, 0, 80) };
+
+            new GildedRose(items).updateQuality();
+
+            assertEquals(0, items[0].sellIn);
+            assertEquals(80, items[0].quality);
         }
     }
 
