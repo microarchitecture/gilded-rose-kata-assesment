f1cd49db463f3d71bf504a1c2e80097d0fe597a8
o.barylo <oleksiib@backbase.com>
2026-03-30 02:17:39 +0300

    feat: Move Normal-related unit tests from GildedRoseTest to NormalUpdateStrategyTest.

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -10,63 +10,6 @@ import org.junit.jupiter.api.Test;
 class GildedRoseTest {
 
     @Nested
-    @DisplayName("Normal Item suite")
-    class NormalItem {
-
-        private static final String NORMAL_ITEM_NAME = "Normal Item";
-
-        @Test
-        void qualityDegradesByOneBeforeSellByDate() {
-            Item[] items = new Item[] { new Item(NORMAL_ITEM_NAME, 5, 10) };
-
-            new GildedRose(items).updateQuality();
-
-            assertEquals(4, items[0].sellIn);
-            assertEquals(9, items[0].quality);
-        }
-
-        @Test
-        void qualityDegradesTwoTimesFasterAtSellInDate() {
-            Item[] items = new Item[] { new Item(NORMAL_ITEM_NAME, 0, 10) };
-
-            new GildedRose(items).updateQuality();
-
-            assertEquals(-1, items[0].sellIn);
-            assertEquals(8, items[0].quality);
-        }
-
-        @Test
-        void qualityDegradesTwoTimesFasterAfterSellByDate() {
-            Item[] items = new Item[] { new Item(NORMAL_ITEM_NAME, -1, 10) };
-
-            new GildedRose(items).updateQuality();
-
-            assertEquals(-2, items[0].sellIn);
-            assertEquals(8, items[0].quality);
-        }
-
-        @Test
-        void qualityMinimumValueIsZero() {
-            Item[] items = new Item[] { new Item(NORMAL_ITEM_NAME, 5, 0) };
-
-            new GildedRose(items).updateQuality();
-
-            assertEquals(4, items[0].sellIn);
-            assertEquals(0, items[0].quality);
-        }
-
-        @Test
-        void qualityMaximumValueIsFifty() {
-            Item[] items = new Item[] { new Item(NORMAL_ITEM_NAME, 5, 50) };
-
-            new GildedRose(items).updateQuality();
-
-            assertEquals(4, items[0].sellIn);
-            assertEquals(49, items[0].quality);
-        }
-    }
-
-    @Nested
     @DisplayName("Sulfuras suite")
     class Sulfuras {
 
