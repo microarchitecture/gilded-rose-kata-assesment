66ee4321e2aa141dcce1e131d5019d82a8953fd9
o.barylo <oleksiib@backbase.com>
2026-03-29 21:33:27 +0300

    feat: improve GildedRose#updateQuality readability.
    Simplify implementation by arranging if-statements per Item kind, getting rid off exclusion if-statements.
    
    Rename Quality change constant with more appropriate name.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -5,11 +5,11 @@ class GildedRose {
     private static final String AGED_BRIE_NAME = "Aged Brie";
     private static final String BACKSTAGE_PASSES_NAME = "Backstage passes to a TAFKAL80ETC concert";
     private static final String SULFURAS_NAME = "Sulfuras, Hand of Ragnaros";
-    private static final int QUALITY_MAX_VALUE = 50;
     private static final int QUALITY_MIN_VALUE = 0;
+    private static final int QUALITY_MAX_VALUE = 50;
+    private static final int QUALITY_DEFAULT_CHANGE = 1;
     private static final int BACKSTAGE_PASSES_FIRST_MILESTONE = 10;
     private static final int BACKSTAGE_PASSES_SECOND_MILESTONE = 5;
-    private static final int QUALITY_NORMAL_CHANGE = 1;
 
     Item[] items;
 
@@ -19,24 +19,22 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!isAgedBrie(item) && !isBackstagePasses(item)) {
-                decrementQualityByOneExceptSulfuras(item);
+            if (isAgedBrie(item)) {
+                incrementQualityByDefault(item);
+            } else if (isBackstagePasses(item)) {
+                incrementQualityByDefault(item);
+                incrementBackstagePassesQuality(item);
             } else {
-                incrementNormalQuality(item);
-                if (isBackstagePasses(item)) {
-                    incrementBackstagePassesQuality(item);
-                }
+                decrementQualityByDefaultExceptSulfuras(item);
             }
             decrementSellInExceptSulfuras(item);
             if (hasSellInDayPassed(item)) {
-                if (!isAgedBrie(item)) {
-                    if (!isBackstagePasses(item)) {
-                        decrementQualityByOneExceptSulfuras(item);
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
+                if (isAgedBrie(item)) {
+                    incrementQualityByDefault(item);
+                } else if (isBackstagePasses(item)) {
+                    item.quality = item.quality - item.quality;
                 } else {
-                    incrementNormalQuality(item);
+                    decrementQualityByDefaultExceptSulfuras(item);
                 }
             }
         }
@@ -58,33 +56,33 @@ class GildedRose {
         return item.sellIn < 0;
     }
 
-    private void decrementQualityByOneExceptSulfuras(Item item) {
+    private void decrementQualityByDefaultExceptSulfuras(Item item) {
         if (item.quality > QUALITY_MIN_VALUE && !isSulfuras(item)) {
-            item.quality = item.quality - QUALITY_NORMAL_CHANGE;
+            item.quality = item.quality - QUALITY_DEFAULT_CHANGE;
         }
     }
 
     private void decrementSellInExceptSulfuras(Item item) {
         if (!isSulfuras(item)) {
-            item.sellIn = item.sellIn - QUALITY_NORMAL_CHANGE;
+            item.sellIn = item.sellIn - 1;
         }
     }
 
-    private void incrementNormalQuality(Item item) {
+    private void incrementQualityByDefault(Item item) {
         if (item.quality < QUALITY_MAX_VALUE) {
-            item.quality = item.quality + QUALITY_NORMAL_CHANGE;
+            item.quality = item.quality + QUALITY_DEFAULT_CHANGE;
         }
     }
 
     private void incrementBackstagePassesQuality(Item item) {
         if (item.sellIn <= BACKSTAGE_PASSES_FIRST_MILESTONE) {
             if (item.quality < QUALITY_MAX_VALUE) {
-                item.quality = item.quality + QUALITY_NORMAL_CHANGE;
+                item.quality = item.quality + QUALITY_DEFAULT_CHANGE;
             }
         }
         if (item.sellIn <= BACKSTAGE_PASSES_SECOND_MILESTONE) {
             if (item.quality < QUALITY_MAX_VALUE) {
-                item.quality = item.quality + QUALITY_NORMAL_CHANGE;
+                item.quality = item.quality + QUALITY_DEFAULT_CHANGE;
             }
         }
     }
