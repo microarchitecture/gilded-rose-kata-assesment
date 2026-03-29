28330d20cce9a6cf2c6987360bc0d95c580383ba
o.barylo <oleksiib@backbase.com>
2026-03-30 02:30:14 +0300

    feat: Refactor GildedRose#updateQuality to delegate all to registry and strategies.
    Add unit test case.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,85 +1,26 @@
 package com.gildedrose;
 
-class GildedRose {
+import com.gildedrose.strategy.ItemUpdateStrategy;
+import com.gildedrose.strategy.ItemUpdateStrategyRegistry;
 
-    private static final String AGED_BRIE_NAME = "Aged Brie";
-    private static final String BACKSTAGE_PASSES_NAME = "Backstage passes to a TAFKAL80ETC concert";
-    private static final String SULFURAS_NAME = "Sulfuras, Hand of Ragnaros";
-    private static final int QUALITY_MIN_VALUE = 0;
-    private static final int QUALITY_MAX_VALUE = 50;
-    private static final int QUALITY_DEFAULT_CHANGE = 1;
-    private static final int BACKSTAGE_PASSES_FIRST_QUALITY_THRESHOLD = 10;
-    private static final int BACKSTAGE_PASSES_SECOND_QUALITY_THRESHOLD = 5;
+public class GildedRose {
 
     Item[] items;
+    private final ItemUpdateStrategyRegistry updateStrategyRegistry;
 
     public GildedRose(Item[] items) {
+        this(items, new ItemUpdateStrategyRegistry());
+    }
+
+    public GildedRose(Item[] items, ItemUpdateStrategyRegistry strategyRegistry) {
         this.items = items;
+        this.updateStrategyRegistry = strategyRegistry;
     }
 
     public void updateQuality() {
         for (Item item : items) {
-            if (isAgedBrie(item)) {
-                incrementQualityByDefault(item);
-            } else if (isBackstagePasses(item)) {
-                incrementBackstagePassesQuality(item);
-            } else {
-                decrementQualityByDefaultExceptSulfuras(item);
-            }
-            decrementSellInExceptSulfuras(item);
-            if (hasSellInDayPassed(item)) {
-                if (isAgedBrie(item)) {
-                    incrementQualityByDefault(item);
-                } else if (isBackstagePasses(item)) {
-                    item.quality = item.quality - item.quality;
-                } else {
-                    decrementQualityByDefaultExceptSulfuras(item);
-                }
-            }
-        }
-    }
-
-    private boolean isAgedBrie(Item item) {
-        return item.name.equals(AGED_BRIE_NAME);
-    }
-
-    private boolean isBackstagePasses(Item item) {
-        return item.name.equals(BACKSTAGE_PASSES_NAME);
-    }
-
-    private boolean isSulfuras(Item item) {
-        return item.name.equals(SULFURAS_NAME);
-    }
-
-    private boolean hasSellInDayPassed(Item item) {
-        return item.sellIn < 0;
-    }
-
-    private void decrementQualityByDefaultExceptSulfuras(Item item) {
-        if (item.quality > QUALITY_MIN_VALUE && !isSulfuras(item)) {
-            item.quality = item.quality - QUALITY_DEFAULT_CHANGE;
-        }
-    }
-
-    private void decrementSellInExceptSulfuras(Item item) {
-        if (!isSulfuras(item)) {
-            item.sellIn = item.sellIn - 1;
-        }
-    }
-
-    private void incrementQualityByDefault(Item item) {
-        if (item.quality < QUALITY_MAX_VALUE) {
-            item.quality = item.quality + QUALITY_DEFAULT_CHANGE;
-        }
-    }
-
-    private void incrementBackstagePassesQuality(Item item) {
-        incrementQualityByDefault(item);
-        if (item.sellIn <= BACKSTAGE_PASSES_FIRST_QUALITY_THRESHOLD) {
-            incrementQualityByDefault(item);
-        }
-        if (item.sellIn <= BACKSTAGE_PASSES_SECOND_QUALITY_THRESHOLD) {
-            incrementQualityByDefault(item);
+            ItemUpdateStrategy strategy = updateStrategyRegistry.getStrategy(item.name);
+            strategy.update(item);
         }
     }
 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,6 @@
 package com.gildedrose;
 
+import static com.gildedrose.ItemNames.AGED_BRIE_NAME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Disabled;
@@ -8,6 +9,24 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class GildedRoseTest {
+
+    @Test
+    void updateQualityAppliesStrategies() {
+        Item[] items = new Item[] {
+            new Item("Unexisting Item", 10, 20),
+            new Item(AGED_BRIE_NAME, 2, 0),
+        };
+
+        new GildedRose(items).updateQuality();
+
+        Item defaultItem = items[0];
+        assertEquals(9, defaultItem.sellIn);
+        assertEquals(19, defaultItem.quality);
+
+        Item agedBrieItem = items[1];
+        assertEquals(1, agedBrieItem.sellIn);
+        assertEquals(1, agedBrieItem.quality);
+    }
 
     @Nested
     @DisplayName("Conjured item suite")
